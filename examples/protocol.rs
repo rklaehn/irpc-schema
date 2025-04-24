d3c8b4e619ea5782933febc8a92717b72a4da760
Ruediger Klaehn <rklaehn@protonmail.com>
2025-04-24 12:15:39 +0200

    Add demo for the migration failure

--- examples/protocol.rs
+++ examples/protocol.rs
@@ -1,10 +1,32 @@
 use anyhow::Result;
 use irpc_schema_derive::{schema, serialize_stable};
 use v1::GetRequest;
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 mod v1 {
-    use serde::{Deserialize, Serialize};
+    use super::*;
+    #[schema(Nominal)]
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct GetRequest {
+        pub key: String,
+    }
 
+    #[schema(Nominal)]
+    #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct PutRequest {
+        pub key: String,
+        pub value: String,
+    }
+
+    #[serialize_stable]
+    #[derive(Debug, PartialEq, Eq)]
+    pub enum Proto {
+        Get(GetRequest),
+        Put(PutRequest),
+    }
+}
+
+mod v2 {
     use super::*;
     #[schema(Nominal)]
     #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -16,17 +38,39 @@ mod v1 {
     #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
     pub struct PutRequest {
         pub key: String,
-        pub value: String,
+        pub value: Option<String>,
     }
 
     #[serialize_stable]
+    #[derive(Debug, PartialEq, Eq)]
     pub enum Proto {
         Get(GetRequest),
         Put(PutRequest),
+        V1Put(v1::PutRequest),
     }
 }
 
+fn roundtrip<T: Serialize, T2: DeserializeOwned>(value: T) -> std::result::Result<T2, postcard::Error> {
+    let bytes = postcard::to_allocvec(&value)?;
+    let value: T2 = postcard::from_bytes(&bytes)?;
+    Ok(value)
+}
+
 fn main() -> Result<()> {
-    <GetRequest as ::irpc_schema::HasSchema>::schema();
+    {
+        let msg = v1::Proto::Get(v1::GetRequest {
+            key: "key".to_string(),
+        });
+        let msg: v2::Proto = roundtrip(msg)?;
+        println!("{:?}", msg);
+    }
+    {
+        let msg = v1::Proto::Put(v1::PutRequest {
+            key: "key".to_string(),
+            value: "value".to_string(),
+        });
+        let msg: v2::Proto = roundtrip(msg)?;
+        println!("{:?}", msg);
+    }
     Ok(())
 }
